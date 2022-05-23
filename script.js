8077b6f3a15b39374279784bb37e8517f28ef5d5
Joseph Wong <josephwong@Josephs-Mac.local>
2022-05-23 14:54:17 -0700

    added HSL conversion

--- script.js
+++ script.js
@@ -36,9 +36,52 @@ var partitionImage = function(context, canvas, rows, columns) {
         }
     }
 
-    console.log(partitionedData);
-    return new Uint8ClampedArray(partitionedData);
-    
+    return new Uint8ClampedArray(partitionedData); // Only care about RGB
+};
+
+var RGBToHSL = function(data) {
+    // https://www.niwa.nu/2013/05/math-behind-colorspace-conversions-rgb-hsl/
+    // Returns HSL values for indexes i, i+1, i+2 and removes alpha
+
+    let HSL = [];
+    for (let i = 0; i < data.length; i += 4) {
+        let r = data[i] / 255;
+        let g = data[i+1] / 255;
+        let b = data[i+2] / 255;
+
+        let min = Math.min(r, g, b);
+        let max = Math.max(r, g, b);
+
+        let l = (min + max) / 2;
+
+        let s = 0;
+        if (l <= 0.5) {
+            s = (max - min)/(max + min);
+        }
+
+        else {
+            s = (max - min)/(2.0 - max - min);
+        }
+
+        let rmax = r == max;
+        let gmax = g == max;
+        let bmax = b == max; // figure out which is the max value
+        console.log(rmax, gmax, bmax, r, g, b);
+        let h = 0;
+        if (rmax) {
+            h = (g - b)/(max - min);
+        }
+        else if (gmax) {
+            h = 2.0 + (b - r)/(max - min);
+        }
+        else {
+            h = 4.0 + (r-g)/(max - min);
+        }
+        HSL.push(h *  60);
+        HSL.push(s);
+        HSL.push(l);
+    }
+    return HSL;
 };
 
 var loadFile = function(event) {
@@ -49,7 +92,8 @@ var loadFile = function(event) {
     image.src = URL.createObjectURL(event.target.files[0]);
     image.onload = function(){
         context.drawImage(image, 0, 0);
-        partitionImage(context, canvas, 10, 10);
+        let data = partitionImage(context, canvas, 10, 10);
+        data = RGBToHSL(data);
     }
 
     // Loading image into Canvas: https://stackoverflow.com/questions/6011378/how-to-add-image-to-canvas
