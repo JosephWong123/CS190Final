864b08c551160b68f54ceda8249800e1928d4e48
rchong3 <56381270+rchong3@users.noreply.github.com>
2022-05-23 21:59:05 -0700

    Add simple sounds

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var audioContext;
 var partitionImage = function(context, canvas, rows, columns) {
     var width = canvas.width;
     var height = canvas.height;
@@ -66,7 +67,6 @@ var RGBToHSL = function(data) {
         let rmax = r == max;
         let gmax = g == max;
         let bmax = b == max; // figure out which is the max value
-        console.log(rmax, gmax, bmax, r, g, b);
         let h = 0;
         if (rmax) {
             h = (g - b)/(max - min);
@@ -77,23 +77,56 @@ var RGBToHSL = function(data) {
         else {
             h = 4.0 + (r-g)/(max - min);
         }
-        HSL.push(h *  60);
-        HSL.push(s);
-        HSL.push(l);
+        HSL.push(isFinite(h) ? (h *  60 + 360) % 360 : 0);
+        HSL.push(isFinite(s) ? s : 0);
+        HSL.push(isFinite(l) ? l : 0);
     }
     return HSL;
 };
 
+var startNote = function(gain, freq, lightness, delay) {
+    const oscillator = audioContext.createOscillator();
+    oscillator.frequency.value = freq;
+    oscillator.type = "sine";
+    const oscillatorGain = audioContext.createGain();
+    oscillator.connect(oscillatorGain);
+    oscillatorGain.connect(audioContext.destination);
+    oscillatorGain.gain.cancelScheduledValues(delay);
+    oscillatorGain.gain.setValueAtTime(0, delay);
+    oscillatorGain.gain.linearRampToValueAtTime(gain, delay + (1 - lightness) * .025);
+    oscillatorGain.gain.linearRampToValueAtTime(0, delay + .48);
+    oscillator.start(delay);
+    oscillator.stop(delay + .5);
+};
+
+var playNotes = function(data) {
+    const freqs = [261.63, 293.67, 329.63, 349.23, 392, 440, 493.89];
+    let oscillators = [];
+    for (let i = 0; i < data.length; i += 3) {
+        const pitch = Math.round(data[i] * 7 / 360) % 7;
+        const gain = data[i+1];
+        const lightness = data[i+2];
+        console.log(pitch, gain, lightness);
+        startNote(gain, freqs[pitch], lightness, i / 3 / 2);
+    }
+};
+
 var loadFile = function(event) {
-	var canvas = document.getElementById('output');
+    if (typeof audioContext !== 'undefined') {
+        audioContext.close();
+    }
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioContext = new AudioContext();
+    var canvas = document.getElementById('output');
     let context = canvas.getContext('2d');
 
     image = new Image();
     image.src = URL.createObjectURL(event.target.files[0]);
     image.onload = function(){
-        context.drawImage(image, 0, 0);
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
         let data = partitionImage(context, canvas, 10, 10);
         data = RGBToHSL(data);
+        playNotes(data);
     }
 
     // Loading image into Canvas: https://stackoverflow.com/questions/6011378/how-to-add-image-to-canvas
