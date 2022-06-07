ae7bea17fc5613d455527dcd6e4bdad0202ab5a0
rchong3 <56381270+rchong3@users.noreply.github.com>
2022-06-07 09:29:22 -0700

    Implement 3-part harmony

--- play-notes.js
+++ play-notes.js
@@ -164,28 +164,102 @@ function playHarmony(HSLdata, RGBdata, key, mode, rootHue) {
 
     let repeatProb = initialRepeatProb;
     let state = maxDuration;
-    let time = 0;
+    let melodyTime = 0;
 
     setUpAudioContext();
     const [keys, freqs] = getKeysFreqs(key, mode);
+    
+    let prevPitch = 0;
 
     for (let i = 0; i < HSLdata.length; i += 3) { // HSLdata and RGBdata are the same length
+        const measureTime = i / 3;
+        const measureLength = 1;
         const data = [RGBdata[i], RGBdata[i+1], RGBdata[i+2]];
-        const pitch = Math.round((HSLdata[i] - rootHue + 360) / 360 * 7) % 7;
+        const root = Math.round((HSLdata[i] - rootHue + 360) / 360 * 7) % 7;
         const gain = HSLdata[i+1];
         const lightness = HSLdata[i+2];
-        
-        if (Math.random() > repeatProb) {
-            state = Math.round(Math.random() * maxDuration);
-            repeatProb = initialRepeatProb;
-        } else {
-            repeatProb /= 2;
+        const attackTime = (1 - lightness) * 0.025;
+
+        // play root in bass
+        playNote(measureTime, measureLength, gain / 3, freqs[root] / 2, attackTime);
+
+        let startPitches = [false, false, false];
+        let endPitches = [false, false, false];
+
+        // play melody
+        while (melodyTime < measureTime + measureLength) {
+            if (Math.random() > repeatProb) {
+                state = Math.round(Math.random() * maxDuration);
+                repeatProb = initialRepeatProb;
+            } else {
+                repeatProb /= 2;
+            }
+
+            const duration = state / subdivision;
+            
+            let pitch;
+            const noteRand = Math.random();
+            if (noteRand < 0.3) {
+                pitch = root;
+            } else if (noteRand < 0.6) {
+                pitch = root + 2;
+            } else if (noteRand < 0.9) {
+                pitch = root + 4;
+            } else if (noteRand < 0.95) {
+                pitch = prevPitch + 1;
+            } else {
+                pitch = prevPitch - 1;
+            }
+            prevPitch = pitch;
+
+            if (melodyTime < measureTime + measureLength / 2) {
+                switch (pitch) {
+                    case root:
+                        startPitches[0] = true;
+                        break;
+                    case root + 2:
+                        startPitches[1] = true;
+                        break;
+                    case root + 4:
+                        startPitches[2] = true;
+                        break;
+                    default:
+                }
+            }
+            if (melodyTime + duration > measureTime + measureLength / 2) {
+                switch (pitch) {
+                    case root:
+                        endPitches[0] = true;
+                        break;
+                    case root + 2:
+                        endPitches[1] = true;
+                        break;
+                    case root + 4:
+                        endPitches[2] = true;
+                        break;
+                    default:
+                }
+            }
+
+            playNote(melodyTime, duration, gain / 3, freqs[(pitch + 7) % 7] * 2, attackTime);
+            melodyTime += duration;
         }
-
-        const duration = state / subdivision;
-
-        [freqs[pitch], freqs[(pitch+2)%freqs.length], freqs[(pitch+4)%freqs.length]].forEach((v) => playNote(time, duration, gain / 3, v, (1 - lightness) * 0.025));
-        time += duration;
+        
+        // fill inner voice
+        if (endPitches[1]) {
+            playNote(measureTime + measureLength / 2, measureLength / 2, gain / 3, freqs[(root + 2) % 7], attackTime);
+        } else if (endPitches[2]) {
+            playNote(measureTime + measureLength / 2, measureLength / 2, gain / 3, freqs[(root + 4) % 7], attackTime);
+        } else {
+            playNote(measureTime + measureLength / 2, measureLength / 2, gain / 3, freqs[root], attackTime);
+        }
+        if (startPitches[1]) {
+            playNote(measureTime, measureLength / 2, gain / 3, freqs[(root + 2) % 7], attackTime);
+        } else if (startPitches[2]) {
+            playNote(measureTime , measureLength / 2, gain / 3, freqs[(root + 4) % 7], attackTime);
+        } else {
+            playNote(measureTime, measureLength / 2, gain / 3, freqs[root], attackTime);
+        }
     }
 }
 
